8f79513783a64dd83c11e215be6100180a246048
Matthew Meyerink <matthew.meyerink@ascentcloud.io>
2021-02-18 11:37:59 -0500

    Adding hero list with details on click feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+export class HeroesComponent {
+  selectedHero?: Hero;
+  heroes = HEROES;
+
   constructor() { }
 
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
