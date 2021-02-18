8b12f6cc1943e33159ecfc5005087fd430037d1a
Matthew Meyerink <matthew.meyerink@ascentcloud.io>
2021-02-18 14:45:37 -0500

    Added message service to display messages across app

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,7 +12,8 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes?: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -19,6 +21,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Heroes Component: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
